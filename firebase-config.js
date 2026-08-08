c271eea637d76eefd9d8b2c18f8985e861cd127b
zumurf-ux <zumurf@gmail.com>
2026-08-08 16:25:55 +0900

    Connect admin page to Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 // Firebase Console > 프로젝트 설정 > 내 앱 > SDK 설정 및 구성 값을 넣으세요.
 // 이 설정값은 웹 클라이언트 식별자이며 비밀키가 아닙니다. 관리자 권한은 Firestore Rules로 보호합니다.
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyBRWlA5RmVNo83hm5QdMwVTieA7RMeQKnk",
+  authDomain: "moment-jamsi.firebaseapp.com",
+  projectId: "moment-jamsi",
+  storageBucket: "moment-jamsi.firebasestorage.app",
+  messagingSenderId: "30188628493",
+  appId: "1:30188628493:web:9cc7d45f09007e8f518344",
 };
 
 export const firebaseRegion = "asia-northeast3";
